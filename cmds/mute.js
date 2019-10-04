570d04d47b3780a0e8fbf7fe7b5a2b183a41a653
0CupTea0 <40626488+0CupTea0@users.noreply.github.com>
2019-10-04 18:00:06 +0700

    Update mute.js

--- cmds/mute.js
+++ cmds/mute.js
@@ -6,11 +6,6 @@ module.exports.run = async (bot,message,args) => {
     if(!args[0]) return bot.send("Вы не указали пользователя");
     if(!rUser) return bot.send("Пользователь не найден");
     if(!args[1]) return bot.send("Укажите время в секундах");
-    let embed = new Discord.RichEmbed()
-    .setDescription("ЗАТКНУЛСЯ")
-    .setColor('#e22216')
-    .addField("Администратор",message.author.username)
-    .addField("Замутил",`${rUser.user.username}`);
     let role = message.guild.roles.find(r => r.name === "Muted");
     if(!role){
         role = await message.guild.createRole({
@@ -23,6 +18,11 @@ module.exports.run = async (bot,message,args) => {
                 ADD_REACTIONS:false
             });
         });
+            let embed = new Discord.RichEmbed()
+    .setDescription("ЗАТКНУЛСЯ")
+    .setColor('#e22216')
+    .addField("Администратор",message.author.username)
+    .addField("Замутил",`${rUser.user.username}`);
     };
     if(rUser.roles.has(role.id)) return bot.send("Этот пользователь уже не может говорить");
     bot.mutes[rUser.id] = {
