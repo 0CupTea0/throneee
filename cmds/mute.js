d478c517474ce42810eeccbc37f729562920c77f
0CupTea0 <40626488+0CupTea0@users.noreply.github.com>
2019-10-04 18:05:11 +0700

    Update mute.js

--- cmds/mute.js
+++ cmds/mute.js
@@ -6,6 +6,17 @@ module.exports.run = async (bot,message,args) => {
     if(!args[0]) return bot.send("Вы не указали пользователя");
     if(!rUser) return bot.send("Пользователь не найден");
     if(!args[1]) return bot.send("Укажите время в секундах");
+    let embed = new Discord.RichEmbed()
+    .setDescription("БАН БЛЯДЬ")
+    .setColor('#e22216')
+    .addField("Администратор",message.author.username)
+    .addField("Забанил",`${rUser.user.username}`);
+    
+    message.guild.member(rUser).ban("Бан");
+    message.channel.send(embed);
+    }catch(err){
+        console.log(`1.${err.name}\n2.${err.message}\n3.${err.stack}`);
+    }
     let role = message.guild.roles.find(r => r.name === "Muted");
     if(!role){
         role = await message.guild.createRole({
@@ -18,11 +29,6 @@ module.exports.run = async (bot,message,args) => {
                 ADD_REACTIONS:false
             });
         });
-            let embed = new Discord.RichEmbed()
-    .setDescription("ЗАТКНУЛСЯ")
-    .setColor('#e22216')
-    .addField("Администратор",message.author.username)
-    .addField("Замутил",`${rUser.user.username}`);
     };
     if(rUser.roles.has(role.id)) return bot.send("Этот пользователь уже не может говорить");
     bot.mutes[rUser.id] = {
