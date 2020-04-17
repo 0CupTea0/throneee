18828440b1f5d31579bbf0fec307b8b4de697f5f
0CupTea0 <40626488+0CupTea0@users.noreply.github.com>
2020-04-17 11:35:36 +0700

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -2,9 +2,9 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     try{
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("У вас нет прав");
-    if(args[0]>100) return bot.send("пошёл нахуй мне похуй(меньше 100 указал быстро!)");
-    if(args[0]<1) return bot.send("нахуй пошёл мне похуй (больше 1 указал сука!");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Пошёл нахуй, у тебя нет прав, уебок");
+    if(args[0]>100) return bot.send("Пошёл нахуй мне похуй(меньше 100 указал быстро!)");
+    if(args[0]<1) return bot.send("Нахуй пошёл мне похуй (больше 1 указал сука!)");
     message.channel.bulkDelete(args[0]).then(() =>{
         message.channel.send(`Удалено ${args[0]} сообщений`).then(msg => msg.delete(15*1000));
     });
