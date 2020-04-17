b3288a5559d1ded1c36cbd29778b5f4d6c737f0e
0CupTea0 <40626488+0CupTea0@users.noreply.github.com>
2020-04-17 11:33:21 +0700

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     try{
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("У вас нет прав");
-    if(args[0]>100) return bot.send("Укажите значение меньше 100");
-    if(args[0]<1) return bot.send("Укажите значение больше 1");
+    if(args[0]>100) return bot.send("пошёл нахуй мне похуй(меньше 100 указал быстро!)");
+    if(args[0]<1) return bot.send("нахуй пошёл мне похуй (больше 1 указал сука!");
     message.channel.bulkDelete(args[0]).then(() =>{
         message.channel.send(`Удалено ${args[0]} сообщений`).then(msg => msg.delete(15*1000));
     });
