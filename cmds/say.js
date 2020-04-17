45b0d96744dc479e09e2889f9804a221f76049f2
0CupTea0 <40626488+0CupTea0@users.noreply.github.com>
2020-04-17 11:38:08 +0700

    Update say.js

--- cmds/say.js
+++ cmds/say.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("У вас нет прав");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Хули ты раскомандовался тут?");
     let botmessage = args.join(" ");
     message.delete().catch();
     bot.send(botmessage);
